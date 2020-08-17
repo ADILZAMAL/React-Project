40e253a29efe0ce335d03bd867aacaba04d208c4
ADILZAMAL <adilzamal@gmail.com>
2020-08-17 17:14:02 +0530

    navbar completed

---
+++ adil-technical/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Service from "./components/Service";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/app.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/service" component={Service} />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
